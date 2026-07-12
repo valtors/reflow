200021d3cfb2634f7178caeafc488717d196046f
Tamish Mhatre <tamishmhatre123@gmail.com>
2026-07-12 13:26:55 +0530

    fix: rewrite preact useResponsiveValue without useSyncExternalStore

--- src/preact/useResponsiveValue.ts
+++ src/preact/useResponsiveValue.ts
@@ -1,6 +1,6 @@
 "use client";
 
-import { useSyncExternalStore } from "preact/hooks";
+import { useEffect, useState } from "preact/hooks";
 import type { BreakpointMap } from "../core/breakpoints.js";
 import { type ResponsiveValue, resolveResponsive } from "../core/responsive-value.js";
 import { useFluidityContext } from "./context.js";
@@ -8,11 +8,21 @@ import { useFluidityContext } from "./context.js";
 export function useResponsiveValue<T, B extends BreakpointMap>(
   value: ResponsiveValue<T, B>,
 ): T | undefined {
-  const { store, system } = useFluidityContext();
-  const snap = useSyncExternalStore(store.subscribe, store.getSnapshot, store.getServerSnapshot);
+  const { store, system } = useFluidityContext<B>();
+  const [width, setWidth] = useState(() =>
+    typeof window === "undefined" ? store.getServerSnapshot().width : store.getSnapshot().width,
+  );
+
+  useEffect(() => {
+    setWidth(store.getSnapshot().width);
+    return store.subscribe(() => {
+      setWidth(store.getSnapshot().width);
+    });
+  }, [store]);
+
   return resolveResponsive(
     system as unknown as Parameters<typeof resolveResponsive<T, B>>[0],
     value,
-    snap.width,
+    width,
   );
 }
